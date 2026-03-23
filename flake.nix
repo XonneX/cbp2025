2473468277f69e040f2a9d836d1d1a81a64cc6f1
XonneX <5417522+XonneX@users.noreply.github.com>
2026-03-23 09:14:26 +0000

    Add nix development flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Development flake for CPB2025";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      x86 = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages."${x86}";
+    in
+    {
+      devShells."${x86}".default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          gcc
+          gnumake
+          zlib
+          gdown
+          python3
+          python3Packages.pandas
+        ];
+
+        # shellHook = 
+        #   git config --local core.hooksPath .githooks/
+        # ;
+
+        # Environment Variables
+        ENV_VAR = "";
+      };
+    };
+}
+
