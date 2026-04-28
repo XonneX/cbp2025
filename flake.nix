f966667c419d2c689cc42389975e2bf2e46593db
XonneX <5417522+XonneX@users.noreply.github.com>
2026-04-28 11:48:21 +0000

    Fix jupyter notebooks

--- flake.nix
+++ flake.nix
@@ -10,25 +10,44 @@
     let
       x86 = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages."${x86}";
+      python = pkgs.python312;
+      pythonPackages = python.pkgs;
+      lib-path = with pkgs; lib.makeLibraryPath [
+        libffi
+        openssl
+        stdenv.cc.cc
+      ];
     in
     {
       devShells."${x86}".default = pkgs.mkShellNoCC {
         packages = with pkgs; [
+          stdenv.cc.cc.lib
+          python
+          pythonPackages.venvShellHook
+
           gcc
           gnumake
           zlib
           gdown
-          python3
-          python3Packages.pandas
           dos2unix
         ];
 
-        # shellHook = 
-        #   git config --local core.hooksPath .githooks/
-        # ;
+        shellHook = ''
+          SOURCE_DATE_EPOCH=$(date +%s)
+          export "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${lib-path}"
+          VENV=.venv
 
-        # Environment Variables
-        ENV_VAR = "";
+          if test ! -d $VENV; then
+            python3.12 -m venv $VENV
+          fi
+          source ./$VENV/bin/activate
+          export PYTHONPATH=`pwd`/$VENV/${python.sitePackages}/:$PYTHONPATH
+          pip install -r requirements.txt
+        '';
+
+        postShellHook = ''
+          ln -sf ${python.sitePackages}/* ./.venv/lib/python3.12/site-packages
+        '';
       };
     };
 }
