d2df3c709f8a4e35855b9ee4f7686f95eba8bb97
XonneX <5417522+XonneX@users.noreply.github.com>
2026-05-11 07:03:45 +0000

    Only rebuild from scratch if label provided

--- scripts/run.sh
+++ scripts/run.sh
@@ -31,12 +31,14 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 
 rm -rf "$PROJECT_ROOT/sample_results/"
-make clean
+
+if [ -z "$LABEL" ]; then
+  make clean
+fi
+
 make
 python "$SCRIPT_DIR/trace_exec_training_list.py" --trace_dir "$PROJECT_ROOT/sample_traces/" --results_dir "$PROJECT_ROOT/sample_results/"
 
 if [ -n "$LABEL" ]; then
   python "$SCRIPT_DIR/append_result.py" "$LABEL"
-else
-  echo "No label provided, skipping append_result.py"
 fi
